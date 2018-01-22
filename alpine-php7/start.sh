23202fb3b6171c29e3ae094a6f71ce4603eb833c
Raphaël Droz <raphael.droz+floss@gmail.com>
2018-01-22 14:19:32 -0300

    added an alpine+php7+Chromium 63 version

---
+++ alpine-php7/start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+exec sh -c "chromium-browser --disable-background-networking  --disable-default-apps  --disable-extensions  --disable-gpu  --disable-sync  --disable-translate  --headless  --hide-scrollbars  --metrics-recording-only  --mute-audio  --no-first-run --remote-debugging-address=${DEBUG_ADDRESS:-0.0.0.0}  --remote-debugging-port=${DEBUG_PORT:-9222} --safebrowsing-disable-auto-update"
+# https://github.com/westy92/headless-chrome-alpine/blob/master/Dockerfile
