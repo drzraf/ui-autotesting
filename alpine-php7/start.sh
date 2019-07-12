c3a22b7cd1c63e30a51e4a660c713858deaf42ef
Raphaël Droz <raphael.droz+floss@gmail.com>
2019-07-12 16:44:47 -0300

    runtime CHROME_OPTS env support

--- alpine-php7/start.sh
+++ alpine-php7/start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-exec sh -c "chromium-browser --disable-background-networking  --disable-default-apps  --disable-extensions  --disable-gpu  --disable-sync  --disable-translate  --headless  --hide-scrollbars  --metrics-recording-only  --mute-audio  --no-first-run --remote-debugging-address=${DEBUG_ADDRESS:-0.0.0.0}  --remote-debugging-port=${DEBUG_PORT:-9222} --safebrowsing-disable-auto-update --no-sandbox"
+# https://peter.sh/experiments/chromium-command-line-switches/
+exec sh -c "chromium-browser --disable-background-networking  --disable-default-apps  --disable-extensions  --disable-gpu  --disable-sync  --disable-translate  --headless  --hide-scrollbars  --metrics-recording-only  --mute-audio  --no-first-run --remote-debugging-address=${DEBUG_ADDRESS:-0.0.0.0}  --remote-debugging-port=${DEBUG_PORT:-9222} --safebrowsing-disable-auto-update --no-sandbox ${CHROME_OPTS}"
 # https://github.com/westy92/headless-chrome-alpine/blob/master/Dockerfile
